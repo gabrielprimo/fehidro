cf3d9d1af0c7233e200834ae9128276f475e95e0
Vivian Santos <vivianmilenapds@outlook.com>
2020-05-17 23:46:01 -0300

    Telas de login, listagem e cadastro de usuario (sem info especifica para cada tipo)

--- fehidro-client/src/main/java/fehidro/control/UsuarioBean.java
+++ fehidro-client/src/main/java/fehidro/control/UsuarioBean.java
@@ -1,10 +1,12 @@
 package fehidro.control;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.List;
 
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
+import javax.faces.model.SelectItem;
 
 import fehidro.model.Usuario;
 import fehidro.rest.client.UsuarioRESTClient;
@@ -15,9 +17,25 @@ public class UsuarioBean implements Serializable {
 	private static final long serialVersionUID = 1L;
 	private Usuario usuario;
 	private List<Usuario> usuarios;
-
-
 	private String consulta;
+	
+	private List<SelectItem> perfisAcesso;
+
+	public UsuarioBean() {
+		this.usuarios = new ArrayList<Usuario>();
+		Usuario teste = new Usuario();
+		teste.setNome("testeeee");
+		teste.setId((long) 2);
+		teste.setLogin("usr.teste"); 
+
+		List<Usuario> testes = new ArrayList<Usuario>();
+		testes.add(teste);
+		testes.add(teste);
+		testes.add(teste); 
+		
+		this.setUsuarios(testes);
+		this.setPerfisAcesso();
+	}
 
 	public Usuario getUsuario() {
 		return usuario;
@@ -34,7 +52,7 @@ public class UsuarioBean implements Serializable {
 	public void setConsulta(String consulta) {
 		this.consulta = consulta;
 	}
-	
+
 	public List<Usuario> getUsuarios() {
 		return usuarios;
 	}
@@ -43,17 +61,30 @@ public class UsuarioBean implements Serializable {
 		this.usuarios = usuarios;
 	}
 	
+	public List<SelectItem> getPerfisAcesso() {
+		 return this.perfisAcesso;
+	}
+	
+	public void setPerfisAcesso() {
+		List<SelectItem> perfis = new ArrayList<>();
+		perfis.add(new SelectItem("1", "Administrador Secretaria Executiva"));
+		perfis.add(new SelectItem("2", "Secretaria(o) Executiva(o)"));
+		perfis.add(new SelectItem("3", "Avaliador CT-PG"));
+
+		this.perfisAcesso = perfis;
+	}
+
 	public String consultar() {
 		UsuarioRESTClient rest = new UsuarioRESTClient();
 		if (consulta != null && !consulta.trim().isEmpty()) {
-			
+
 		}
 		else {
 			//this.setUsuarios(rest.findAll());
 		}
 		return null;
 	}
-	
+
 	public String gravar() {
 		UsuarioRESTClient rest = new UsuarioRESTClient();
 		if (usuario.getId() == null) {
--- fehidro-client/src/main/java/fehidro/model/Usuario.java
+++ fehidro-client/src/main/java/fehidro/model/Usuario.java
@@ -1,8 +1,17 @@
 package fehidro.model;
 
+import java.util.Date;
+
 public class Usuario {
 	private Long id;
 	private String nome;
+	private String sobrenome;
+	private String CPF;
+	private Date dataNascimento;
+	private int idPerfilAcesso;
+	private String celular;
+	private String login;
+	private String senha;
 	
 	
 	public Long getId() {
@@ -17,5 +26,47 @@ public class Usuario {
 	}
 	public void setNome(String nome) {
 		this.nome = nome;
+	}
+	public String getLogin() {
+		return login;
+	}
+	public void setLogin(String login) {
+		this.login = login;
+	}
+	public String getSenha() {
+		return senha;
+	}
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+	public String getSobrenome() {
+		return sobrenome;
+	}
+	public void setSobrenome(String sobrenome) {
+		this.sobrenome = sobrenome;
+	}
+	public String getCPF() {
+		return CPF;
+	}
+	public void setCPF(String cPF) {
+		CPF = cPF;
+	}
+	public int getIdPerfilAcesso() {
+		return idPerfilAcesso;
+	}
+	public void setIdPerfilAcesso(int idPerfilAcesso) {
+		this.idPerfilAcesso = idPerfilAcesso;
+	}
+	public Date getDataNascimento() {
+		return dataNascimento;
+	}
+	public void setDataNascimento(Date dataNascimento) {
+		this.dataNascimento = dataNascimento;
+	}
+	public String getCelular() {
+		return celular;
+	}
+	public void setCelular(String celular) {
+		this.celular = celular;
 	}
 }
