2b12a62d314297945022a1cf9a96406540ce5860
Cesar Augusto Oliveira <caoliver@users.noreply.github.com>
2020-05-27 23:45:34 -0300

    Updade API
    Update Proposta

---
+++ fehidro-api/src/main/java/br/unisantos/fehidro/model/dao/PropostaDAO.java
@@ -0,0 +1,30 @@
+package br.unisantos.fehidro.model.dao;
+
+import java.util.List;
+import br.unisantos.fehidro.model.Proposta;
+public class PropostaDAO extends DAOFactory<Proposta>{
+
+	public PropostaDAO() {
+		super(Proposta.class);
+	}
+	
+	public void cadastrar(Proposta proposta) {
+		adicionar(proposta);
+	}
+	
+	public void atualizar(Proposta proposta) {
+		alterar(proposta);
+	}
+	
+	public void remover(long id) {
+		excluir(id);
+	}
+
+	public Proposta obter(long id) {
+		return consultarGenerico("Proposta.consultarPorId", id);
+	}
+
+	public List<Proposta> listar() {
+		return listarGenerico("Proposta.listarTodos");
+	}
+}
